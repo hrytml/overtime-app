4823d4a09e79a10ddb0ad20bd76901e9c1c7c1e0
Huriye Arslan Temel <hryearsln@gmail.com>
2017-08-24 16:17:47 +0300

    Implemented edit and update functionality

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@
   
   RSpec.describe User, type: :model do
     before do
-        @user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
+        @user = FactoryGirl.create(:user)
     end
 
     describe "creation" do
